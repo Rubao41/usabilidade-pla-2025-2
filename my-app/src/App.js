ac4264f09520f1e8abdc8bbb23ca7b0a7cd9f962
Ruben Luca Ribeiro Silva <824116967@ulife.com.br>
2025-10-13 11:44:17 -0300

    Feat: create react-bootstrap aplication

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Home from './components/Home'
+
+function App() {
+  const title = "Welcome to react-bootstrap project"
+  const links = [
+    {label: 'Cards', href: "cards.html"},
+    {label: 'Botões', href: "buttons.html"},
+    {label: 'Formulários', href: "forms.html"},
+    {label: 'Navbar', href: "navbar.html"},
+    {label: 'Layout', href: "layout.html"}
+  ]
+  return (
+    <div>
+      <Home title={title} links={links}/>
+    </div>
+  );
+}
+
+export default App;
