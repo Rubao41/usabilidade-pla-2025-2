83bf45f02d2b190858f7c73bac423a06d86ed8c4
Ruben Luca Ribeiro Silva <824116967@ulife.com.br>
2025-10-13 11:07:40 -0300

    chore

--- meu-primeiro-app-react/src/index.js
+++ meu-primeiro-app-react/src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
+import './styles.css';
 
 const App = () => {
+  const ButtonStyle = {
+    marginTop: 12,
+    paddingTop: 8,
+    paddingBottom: 8,
+    backgroundColor: 'blueviolet',
+    color: 'white',
+    border: 'none',
+    width: '100%',
+    borderRadius: 8
+  };
+
+  const labelText = "Nome:";
   return <div style= {{margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
-    <label for="nome" style= {{display: 'block', marginBottom: 4}}>Nome:</label>
+    <label className="label" htmlFor="nome" style= {{display: 'block', marginBottom: 4}}>{labelText}</label>
     <input type="text" id="nome" style= {{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', width: '100%', borderRadius: 8, outline: 'none', boxSizing: 'border-box'}}/>
-    <button style= {{marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'rgb(82, 2, 55)', color: 'white', border: 'none', width: '100%', borderRadius: 8}}>Enviar</button>
+    <button style= {ButtonStyle}>Enviar</button>
     </div>
 }
 
